fabc2f6cc723d13505c4e9c42a241a2318990ea4
Kotryna Guci <kotrynaguci@Kotrynas-MacBook-Pro.local>
2024-03-24 16:06:42 -0500

    signup

---
+++ client/resources/scripts/signin.js
@@ -0,0 +1,34 @@
+function handleOnLoad() {
+    signInDisplay();
+}
+
+function signInDisplay() {
+    let html = `
+    <div class="wrapper">
+        <form action="">
+            <h1>Adopt a Pet Login</h1>
+            <div class="input-box">
+                <input type="text" placeholder="Username" required>
+                <box-icon type='solid' name='user'></box-icon>
+            </div>
+            <div class="input-box">
+                <input type="password" placeholder="Password" required>
+                <box-icon name='lock-alt' type='solid'></box-icon>
+            </div>
+            <div class="remember-forgot">
+                <label><input type="checkbox">Remember Me</label>
+            </div>
+
+            <button type="submit" class="btn btn-danger">Login</button>
+
+            <div class="register-link">
+            <p>Don't Have An Account? <a href="signup.html">Register</a></p>
+            </div>
+        </form>
+    </div>
+    `;
+    document.getElementById('app').innerHTML = html;
+}
+
+
+
